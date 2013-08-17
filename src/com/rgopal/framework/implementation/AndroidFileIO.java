fb09cd50828a478ff3042bb7cd296e634b5f2d2d
Rohit Gopal <rohitgopal@gmail.com>
2013-08-17 14:38:56 -0400

    Filled in AndroidFileIO

--- src/com/rgopal/framework/implementation/AndroidFileIO.java
+++ src/com/rgopal/framework/implementation/AndroidFileIO.java
@@ -1,41 +1,50 @@
 package com.rgopal.framework.implementation;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.res.AssetManager;
+import android.os.Environment;
+import android.preference.PreferenceManager;
 
 import com.rgopal.framework.FileIO;
 
 public class AndroidFileIO implements FileIO {
+	Context context;
+	AssetManager assets;
+	String externalStoragePath;
 
-	public AndroidFileIO(AndroidGame androidGame) {
-		// TODO Auto-generated constructor stub
+	public AndroidFileIO(Context context) {
+        this.context = context;
+        this.assets = context.getAssets();
+        this.externalStoragePath = Environment.getExternalStorageDirectory()
+                .getAbsolutePath() + File.separator;
 	}
 
 	@Override
 	public InputStream readFile(String file) throws IOException {
-		// TODO Auto-generated method stub
-		return null;
+		return new FileInputStream(externalStoragePath + file);
 	}
 
 	@Override
 	public OutputStream writeFile(String file) throws IOException {
-		// TODO Auto-generated method stub
-		return null;
+		return new FileOutputStream(externalStoragePath + file);
 	}
 
 	@Override
 	public InputStream readAsset(String file) throws IOException {
-		// TODO Auto-generated method stub
-		return null;
+		return assets.open(file);
 	}
 
 	@Override
 	public SharedPreferences getSharedPref() {
-		// TODO Auto-generated method stub
-		return null;
+		return PreferenceManager.getDefaultSharedPreferences(context);
 	}
 
 }
